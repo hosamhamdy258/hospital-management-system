60391c44aed7722210fb6b63633cf7daae07aba3
mahaElmorsy <mahaosmanelmorsy@gmail.com>
2022-08-18 03:59:02 +0200

    dep card adjust

--- hms_front/src/components/Departments/DepartmentCard.jsx
+++ hms_front/src/components/Departments/DepartmentCard.jsx
@@ -3,16 +3,14 @@ import { Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import React from 'react'
 
-const DepartmentCard = ({title , logo_img ,id}) => {
+const DepartmentCard = ({name , logo_img ,id}) => {
   return (
-    <Col sm={6} md={4}>
+    <Col  lg='4' sm="6" md="6">
         <div className="hDepts-imgbx">
           <img src={logo_img} alt=""  />
           <div className="hDepts-txtx">
-            <h4>{title}</h4>
-            <button onClick={()=>console.log('appointment')}>Read More <ArrowRightCircle size={15}/> </button>
-            <Link className="dep_link" to={`/hDepts/${id}`} d_id={id}><span className="dept-readmore">read more<i className="fa fa-arrow-right px-2" aria-hidden="true"></i>
-            </span>
+            <h3>{name}</h3>
+            <Link className="depCard_link" to={`/hDepts/${id}`} d_id={id}>read more <ArrowRightCircle size={15}/>
             </Link>
           </div>
         </div>
