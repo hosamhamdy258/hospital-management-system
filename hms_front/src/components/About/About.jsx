87d0a63613541a1f3ab5e8da5d52ac7a25db1665
mahaElmorsy <mahaosmanelmorsy@gmail.com>
2022-08-20 01:03:12 +0200

    adjust aboutus motion

--- hms_front/src/components/About/About.jsx
+++ hms_front/src/components/About/About.jsx
@@ -33,9 +33,9 @@ const About = () => {
             <section className='features'>
                 <div className="container">
                     <div className="row">
-                        <h2 className="about_title">Our Services</h2>
+                        <motion.h2 className="about_title" initial={{x:-100}} whileInView={{x:0 }} transition={{duration:.5}}>Our Services</motion.h2>
 
-                        <motion.div initial={{y:400}} whileInView={{y:0 }} transition={{duration:.5}} className="col-lg-3 col-md-6 d-flex justify-content-between">
+                        <motion.div initial={{x:-50}} whileInView={{x:0 }} className="col-lg-3 col-md-6 d-flex justify-content-between">
                             <Card >
                                 <Card.Img variant="top" src="/features/doctors.jpg" />
                                 <Card.Body>
@@ -47,7 +47,7 @@ const About = () => {
                                 </Card.Body>
                             </Card>
                         </motion.div>
-                        <motion.div initial={{y:400}} whileInView={{y:0 }} transition={{duration:.6}} className="col-lg-3 col-md-6 d-flex justify-content-between">
+                        <motion.div initial={{x:-100}} whileInView={{x:0 }} className="col-lg-3 col-md-6 d-flex justify-content-between">
                             <Card >
                                 <Card.Img variant="top" src="/features/donationbox1.jpg" />
                                 <Card.Body>
@@ -59,7 +59,7 @@ const About = () => {
                                 </Card.Body>
                             </Card>
                         </motion.div>
-                        <motion.div initial={{y:400}} whileInView={{y:0 }} transition={{duration:.7}} className="col-lg-3 col-md-6 d-flex justify-content-between">
+                        <motion.div initial={{x:-150}} whileInView={{x:0 }} className="col-lg-3 col-md-6 d-flex justify-content-between">
                             <Card >
                                 <Card.Img variant="top" src="/features/medical-counseling.jpg" />
                                 <Card.Body>
@@ -71,7 +71,7 @@ const About = () => {
                                 </Card.Body>
                             </Card>
                         </motion.div>
-                        <motion.div initial={{y:400}} whileInView={{y:0 }} transition={{duration:.8}} className="col-lg-3 col-md-6 d-flex justify-content-between">
+                        <motion.div initial={{x:-200}} whileInView={{x:0 }} className="col-lg-3 col-md-6 d-flex justify-content-between">
                             <Card >
                                 <Card.Img variant="top" src="/features/Children-Healthcare-1920x1080-1.jpg" />
                                 <Card.Body>
