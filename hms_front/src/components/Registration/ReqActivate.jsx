2144973ccdb5789132dc345009ddfd254f44c98c
khalid shaheen <khalid.shaheen254@gmail.com>
2022-09-02 15:03:44 +0200

    ReqActivate file edit

--- hms_front/src/components/Registration/ReqActivate.jsx
+++ hms_front/src/components/Registration/ReqActivate.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function reqActivate() {
     return (
         <div className='my-5 container mx-auto'>
-            <h1 className='text-danger'>You Have to go to your Email to varify Your Account  :)</h1>
+            <h1 className='text-danger'>You Have to go to your Email to Activate Your Account  :)</h1>
         </div>
     )
 }
